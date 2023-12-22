25a6a43bad4ef0573a867098cc84305ec87ab7d5
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-12-22 22:46:34 +0800

    Polished

--- pages/MainPage.tsx
+++ pages/MainPage.tsx
@@ -1,11 +1,18 @@
+"use client"
+
 import React from "react";
 import Page from "../components/sub/Page";
 import "../app/globals.css"
+import TransitionEffect from "@/components/sub/TransitionEffect";
 const MainPage = () => {
   return (
+    <>
+    <TransitionEffect/>
     <Page>
       <h1>Home</h1>
     </Page>
+
+    </>
   );
 };
 
