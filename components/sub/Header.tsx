70f47d188fe19719b1278dd87ea2740380935af8
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-12-23 21:35:40 +0800

    Navbar

--- components/sub/Header.tsx
+++ components/sub/Header.tsx
@@ -3,20 +3,24 @@ import Link from 'next/link'
 import "../../app/globals.css"
 const Header = () => {
   return (
-  <header className='bg-slate-600'>
-    <div className='w-full flex flex-rows gap-7 justify-center'>
+  <header className='bg-slate-600 w-full h-14 flex flex-row justify-center items-center'>
+    <div className='w-4/12 flex-grow px-10'>
+            <h1 className='font-bold'>Justin Santos</h1>
+    </div>
+    <div className='flex w-4/12 flex-rows gap-20 justify-center items-center pr-40 md:pr-5'>
+
         <div className="header-item ">
-            <Link href={"/MainPage"}>
+            <Link href={"/MainPage"} className='text-2xl font-bold'>
                 Home
             </Link>
         </div>
         <div className="header-item">
-            <Link href={"/About"}>
+            <Link href={"/About"} className='text-2xl font-bold'>
                 About
             </Link>
         </div>
         <div className="header-item">
-            <Link href={"/Contact"}>
+            <Link href={"/Contact"} className='text-2xl font-bold'>
                Contact
             </Link>
         </div>
